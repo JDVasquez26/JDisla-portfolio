1c83d432b339dfe84b7ae524ee785423369521e1
JDVasquez26 <114019007+JDVasquez26@users.noreply.github.com>
2023-02-26 21:31:02 -0500

    created components,snap scrolling, topbar and menu, dynamic action to menu

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './topbar.scss'
+
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function Topbar( {menuOpen, setMenuOpen} ) {
+  return (
+    //concatnating 'topbar active' when hamburger is clicked it'll have set opposite state
+    <div className={'topbar ' + (menuOpen && 'active')} > 
+        <div className="wrapper">
+            <div className="left">
+                <a href="#intro" className='logo'>jesthine.</a>
+                <div className="itemContainer">
+                    <PersonIcon className='icon'/>
+                    <span>201 589 0721</span>
+                </div>
+                <div className="itemContainer">
+                    <MailIcon className='icon'/>
+                    <span>jesthinedisla@gmail.com</span>
+                </div>
+            </div>
+            <div className="right">
+                <div className="hamburger" onClick={() =>setMenuOpen(!menuOpen)}>
+                    <span className='line1'></span>
+                    <span className='line2'></span>
+                    <span className='line3'></span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
